521e67e3da3d47bb1be44a7632ae01ef8e2ab816
Methmal1001 <hau.methmal1@gmail.com>
2026-04-27 10:34:03 +0530

    Initial ERP frontend setup with Nuxt 4

---
+++ pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="h-screen flex items-center justify-center bg-gray-100">
+    <h1 class="text-4xl font-bold text-blue-600">
+      System Ready 🚀
+    </h1>
+  </div>
+</template>
